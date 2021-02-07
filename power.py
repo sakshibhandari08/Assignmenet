53d9e5ed94d8f4db767d3ecaafd21129f9aec00c
sakshibhandari08 <78673224+sakshibhandari08@users.noreply.github.com>
2021-02-07 09:41:55 +0530

    Day 1 Assignment Submitted question 2

---
+++ power.py
@@ -0,0 +1,5 @@
+#Write a python program that takes two numbers as the input such as X and Y and print the result
+#of X^Y(X to the power of Y).
+x=int(input("Enter first number"))
+y=int(input("Enter second number"))
+print("x^y(x to the power y ) is",pow(x,y))
