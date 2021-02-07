242bc205b643d0d7b95b7689cbd1d2d9effe4c76
sakshibhandari08 <78673224+sakshibhandari08@users.noreply.github.com>
2021-02-07 09:34:42 +0530

    Day 1 Assignment Submitted

---
+++ twonumbers.py
@@ -0,0 +1,9 @@
+#Write a python program that takes two numbers as the input and print the result of Addition,
+#Subtraction, Multiplication, Division of the two numbers
+
+a= int(input("Enter first number "))
+b=int(input("Enter second number "))
+print("Addition of two numbers is ",a+b)
+print("Subtraction of two number is ",a-b)
+print("Multiplication of two number is ",a*b)
+print("Division of two number is ",a/b)
