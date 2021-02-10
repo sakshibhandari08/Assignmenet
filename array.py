4455fe0ea3366e91629734a4a4b49518494de0c0
sakshibhandari08 <78673224+sakshibhandari08@users.noreply.github.com>
2021-02-10 12:46:48 +0530

    Assignment 3

--- array.py
+++ array.py
@@ -1,3 +1,16 @@
+#Question 1
+#Create an Array in python using the list and perform delete from any position operation
 array=[50,60,70,80,90,100]
 array.remove(60)
 print(array)
+
+#Question 2
+#What is constructor? Mention the task of the constructor.
+#constructor is a special type of method (function).
+# It is used to create an object.
+#Constructors can be of two types.
+#Parameterized Constructor
+#Non-parameterized Constructor
+#In python --init__() generates the constructor of class it is called
+#when class is instantiated,it accepts self keyeprd as first argument.
+#the constructor is to ensure that the new object is in a valid state
